b2c725436c6225b17ed21dc37ddf90d485e77661
fsshoily <farihasabrinshoily@gmail.com>
2026-07-20 15:57:38 +0600

    Sheet-2-U_Some_Sums

---
+++ Competetive Programming/ASSUIT ICPC Prep/Shoily/Sheet-02/U_Some_Sums.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,A,B,total = 0; cin>>N>>A>>B;
+    for(int i=1;i<=N;i++)
+    {
+        int sum=0;
+        int temp=i;
+        while(temp!=0)
+        {
+            int t=temp%10;
+            sum+=t;  
+            temp/=10;   
+        }
+         if(sum>=A && sum<=B)
+            {
+                total+=i;
+            }     
+    }
+    cout<<total;
+}
